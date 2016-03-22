87478be6bc14c93175372043a3b07160d0e67a2b
PedroPinheiro <pedropaulocspinheiro@gmail.com>
2016-03-21 23:04:15 -0300

    index.js start test

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-
-let fenix = () => {
-	return "00"
-}
+import Fenix from "./src/Fenix"
 
 
-export default fenix
+let urlBase = "http://jsonplaceholder.typicode.com"
+let sources = {
+	"posts": {
+		methods: '*'
+	},
+	"comments": {
+		methods: 'GET',
+		cache: {
+			expires: "30"
+		}
+	}
+};
+
+
+let api = new Fenix({ urlBase, sources });
+
+
+
+(async function() {
+
+	let comment = await api.comments(1);
+
+	console.log(comment);
+
+	let comment2 = await api.comments(1);
+
+	console.log(comment2);
+
+}());
