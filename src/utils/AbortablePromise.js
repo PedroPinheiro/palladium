1cb2414bfaaed50d5d2d0eb1e4267145c707d03f
Pedro Pinheiro <pedro.pinheiro@kpl.com.br>
2016-05-26 19:17:21 -0300

    fix(AbortablePromise): inheritance to composition

--- src/utils/AbortablePromise.js
+++ src/utils/AbortablePromise.js
@@ -1,21 +1,23 @@
 
-class AbortablePromise extends Promise {
-    abort() {
-        return (this._abort)?this._abort():null;
-    }
-    setAbort(abort) {
-        this._abort = abort;
-    }
-    then(params) {
-        let p = super.then(params);
-        p._abort = this._abort;
-        return p;
-    }
-    catch(params) {
-        let p = super.catch(params);
-        p._abort = this._abort;
-        return p;
-    }
+class AbortablePromise {
+
+     constructor(params) {
+       this._promise = new Promise(params);
+     }
+     abort() {
+       return (this._abort)?this._abort():null;
+     }
+     setAbort(abort) {
+       this._abort = abort;
+     }
+     then(params) {
+       this._promise.then(params);
+       return this;
+     }
+     catch(params) {
+       this._promise.catch(params);
+       return this;
+     }
 }
 
 export default AbortablePromise
