ca5e520484dff3cb5709fa7dca3078ea8f21a858
Pedro Pinheiro <pedro.pinheiro@kpl.com.br>
2016-06-16 15:30:04 -0300

    Release

--- dummy/entry.js
+++ dummy/entry.js
@@ -1,7 +1,8 @@
 import Fenix from "fenixjs";
 
 
-const url = "http://jsonplaceholder.typicode.com";
+// const url = "http://jsonplaceholder.typicode.com";
+const url = "https://api.express.kpl.com.br/api";
 
 
 const defaults = {
@@ -11,33 +12,69 @@ const defaults = {
     // }
 };
 
+let xAuthToken = "";
 
 const config = {
-    "posts": {
-        methods: '*',
-        // Example of endpoint options
-        // options: {
-        //     pk: "idPost"
-        // },
-        "comments": {
-            methods: '*',
-            cache: {
-                expires: "30",
-                // type: LocalStorageCache
+    "accounts": {
+        methods: 'GET',
+        "login": {
+            methods: 'POST',
+            options: {
+                responseHeaders: {
+                    set "X-AUTH-TOKEN" (token) {
+                        xAuthToken = token;
+                    }
+                }
             }
-        },
-        // example of Service
-        "send": {
-            methods: "POST"
         }
     },
-    "comments": {
-        methods: 'GET',
-        cache: {
-            expires: "30"
+    "users": {
+        methods: '*',
+        "me": {
+            methods: 'GET',
+            options: {
+                requestHeaders: {
+                    get "X-AUTH-TOKEN" (){
+                        return xAuthToken;
+                    }
+                }
+            }
         }
     }
-};
+}
+
+//
+// const config = {
+//     "posts": {
+//         methods: '*',
+//         // Example of endpoint options
+//         // options: {
+//         //     pk: "idPost"
+//         // },
+//         "comments": {
+//             methods: '*',
+//             cache: {
+//                 expires: "30",
+//                 // type: LocalStorageCache
+//             }
+//         },
+//         // example of Service
+//         "send": {
+//             methods: "POST"
+//         },
+//         options: {
+//             requestHeaders: {
+//                 "x-auth-token": "eyJleHBpcmVzIjoxNDY1NjQ3MTE1MjQ3LCJ1c2VybmFtZSI6InBlZHJvQHBpbmhlaXJvIn0=.Sis7a3R8PV9BqKAZLeedbxaG2KKlkNa2J6eKAsRzkhQ="
+//             }
+//         }
+//     },
+//     "comments": {
+//         methods: 'GET',
+//         cache: {
+//             expires: "30"
+//         }
+//     }
+// };
 
 
 const api = new Fenix({ url, config, defaults });
--- dummy/server.js
+++ dummy/server.js
@@ -5,6 +5,7 @@ var config = require('./webpack.config');
 var app = express();
 var compiler = webpack(config);
 
+
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath,
@@ -19,17 +20,18 @@ app.use(require('webpack-hot-middleware')(compiler, {
   log: console.log
 }));
 
+
 app.get('/', function (request, response){
     require('fs').readFile('./index.html', 'utf-8', function(err, data) {
         response.send(data);
     });
 })
 
-app.listen(3030, 'localhost', function(err) {
+app.listen(3080, 'localhost', function(err) {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:3030');
+  console.log('Listening at http://localhost:3080');
 });
