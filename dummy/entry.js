b457a2e56c22f9e6e877eacf535909757cdc544e
Pedro Pinheiro <pedro.pinheiro@kpl.com.br>
2016-08-02 10:39:02 -0300

    chore(dummy): resolving palladium dependency

--- dummy/entry.js
+++ dummy/entry.js
@@ -2,107 +2,50 @@ import Palladium from "palladium";
 
 
 // const url = "http://jsonplaceholder.typicode.com";
-// const url = "https://api.express.kpl.com.br/api";
 const url = "http://localhost:3040";
 
+let xAuthToken = "";
 
 const defaults = {
-    // example of api default options
-    // options: {
-    //     pk: "_id"
-    // }
+    responseHeaders: {
+        set "X-AUTH-TOKEN" (token) {
+            xAuthToken = token;
+        }
+    }
 };
 
-let xAuthToken = "";
+const loginOptions = {
+    responseHeaders: {
+        set "X-AUTH-TOKEN" (token) {
+            xAuthToken = token;
+        }
+    }
+}
+
 
 const config = {
     "login": {
         methods: 'POST',
-        options: {
-            responseHeaders: {
-                set "X-AUTH-TOKEN" (token) {
-                    xAuthToken = token;
-                }
-            }
-        }
+        options: loginOptions
     },
     "accounts": {
         methods: '*',
         "login": {
             methods: 'POST',
-            options: {
-                responseHeaders: {
-                    set "X-AUTH-TOKEN" (token) {
-                        xAuthToken = token;
-                    }
-                }
-            }
+            options: loginOptions
         },
     },
     "products": {
-        methods: '*',
-        options: {
-            requestHeaders: {
-                get "X-AUTH-TOKEN" (){
-                    return xAuthToken;
-                }
-            }
-        }
+        methods: '*'
     },
     "brands": {
-        methods: '*',
-        options: {
-            requestHeaders: {
-                get "X-AUTH-TOKEN" (){
-                    return xAuthToken;
-                }
-            }
-        }
+        methods: '*'
     },
     "posts": {
-        methods: '*',
-        options: {
-            requestHeaders: {
-                get "X-AUTH-TOKEN" (){
-                    return xAuthToken;
-                }
-            }
-        }
+        methods: '*'
     },
 }
 
-//
-// const config = {
-//     "posts": {
-//         methods: '*',
-//         // Example of endpoint options
-//         // options: {
-//         //     pk: "idPost"
-//         // },
-//         "comments": {
-//             methods: '*',
-//             cache: {
-//                 expires: "30",
-//                 // type: LocalStorageCache
-//             }
-//         },
-//         // example of Service
-//         "send": {
-//             methods: "POST"
-//         },
-//         options: {
-//             requestHeaders: {
-//                 "x-auth-token": "eyJleHBpcmVzIjoxNDY1NjQ3MTE1MjQ3LCJ1c2VybmFtZSI6InBlZHJvQHBpbmhlaXJvIn0=.Sis7a3R8PV9BqKAZLeedbxaG2KKlkNa2J6eKAsRzkhQ="
-//             }
-//         }
-//     },
-//     "comments": {
-//         methods: 'GET',
-//         cache: {
-//             expires: "30"
-//         }
-//     }
-// };
 
 
 const api = new Palladium({ url, config, defaults });
